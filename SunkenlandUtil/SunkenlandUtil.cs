75c6a314fc7326c30713477ddc45c3d7192a4518
satroki <satroki@outlook.com>
2023-09-13 03:08:26 +0800

    添加项目文件。

---
+++ SunkenlandUtil/SunkenlandUtil.cs
@@ -0,0 +1,313 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using HarmonyLib.Tools;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityGameUI;
+using static UnityEngine.Random;
+
+namespace SunkenlandUtil
+{
+    [BepInPlugin("satroki.sunkenland.util", "Util Plugin", "0.0.7")]
+    public class SunkenlandUtil : BaseUnityPlugin
+    {
+        private readonly Harmony _harmony = new Harmony("satroki.sunkenland.util");
+        public static ManualLogSource _logger;
+        private static bool worldSensor = false;
+        private static bool scanOre;
+        private static int sensorSpan;
+        private static bool returnBottle = false;
+        private static Text text;
+        private static GameObject uiArrow;
+        private static RectTransform arrowTransform;
+        private static int fcnt;
+        private static Component nearestObj;
+        private static GameObject uiPanel;
+        private static GameObject uiText;
+        private static ConfigFile config;
+
+        private void Awake()
+        {
+            _logger = Logger;
+
+            config = Config;
+            InitConfig();
+            try
+            {
+                _harmony.PatchAll(typeof(SunkenlandUtil));
+                _logger.LogInfo("UtilPlugin is loaded!");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+            }
+        }
+
+        private static void InitConfig()
+        {
+            LoadConfig.Init(config);
+            worldSensor = LoadConfig.WorldSensor.Value;
+            scanOre = LoadConfig.ScanOre.Value;
+            sensorSpan = LoadConfig.SensorSpan.Value;
+            returnBottle = LoadConfig.ReturnBottle.Value;
+            _logger.LogInfo("UtilPlugin InitConfig");
+        }
+
+        [HarmonyPatch(typeof(RM), "LoadResources")]
+        [HarmonyPostfix]
+        public static void LoadResources(ref RM __instance)
+        {
+            var m = LoadConfig.StackAmount.Value;
+            foreach (var item in __instance.ItemDictionary.Values)
+            {
+                if (item.stackAmount > 1)
+                    item.stackAmount *= m;
+            }
+            _logger.LogInfo($"LoadResources Change StackAmount");
+        }
+
+
+        [HarmonyPatch(typeof(PlayerCharacter), "CalculatePlayerStats")]
+        [HarmonyPostfix]
+        public static void CalculatePlayerStats(ref PlayerCharacter __instance, ref Storage ___playerStorage)
+        {
+            __instance.AirConsumtionRate *= LoadConfig.AirConsumtionRate.Value;
+            __instance.EnergyConsumptionRate *= LoadConfig.EnergyConsumptionRate.Value;
+            __instance.StaminaRecoveryRate *= LoadConfig.StaminaRecoveryRate.Value;
+
+            Traverse.Create(__instance).Property(nameof(PlayerCharacter.DefenceBody)).SetValue(__instance.DefenceBody + LoadConfig.Defence.Value);
+            Traverse.Create(__instance).Property(nameof(PlayerCharacter.DefenceHead)).SetValue(__instance.DefenceHead + LoadConfig.Defence.Value);
+            ___playerStorage.MaxItemsAmount += LoadConfig.MaxItemsAmount.Value;
+        }
+
+        [HarmonyPatch(typeof(PlayerCharacter), "DamageArmor")]
+        [HarmonyPrefix]
+        public static bool DamageArmor(ref float point, ref int type)
+        {
+            return LoadConfig.DamageArmor.Value;
+        }
+
+        [HarmonyPatch(typeof(UICombat), "Update")]
+        [HarmonyPostfix]
+        public static void UICombatUpdate(ref UICombat __instance)
+        {
+            if (worldSensor && (bool)WorldScene.code && (bool)Global.code.Player)
+            {
+                fcnt++;
+                if (uiPanel == null)
+                {
+                    CreateUI();
+                    _logger.LogInfo($"Create UIText");
+                }
+                if (fcnt >= sensorSpan)
+                {
+                    text.text = GetNearestObject(Global.code.Player.transform.position);
+                    fcnt = 0;
+                    uiPanel.SetActive((bool)nearestObj);
+                }
+                UpdateArrow();
+            }
+        }
+
+
+        [HarmonyPatch(typeof(WorldManager), "OnLogoutFromSceneWorld")]
+        [HarmonyPostfix]
+        public static void OnLogoutFromSceneWorld()
+        {
+            uiPanel?.SetActive(false);
+        }
+
+        [HarmonyPatch(typeof(WorldManager), "OnWorldSceneLoaded")]
+        [HarmonyPostfix]
+        public static void OnWorldSceneLoaded()
+        {
+            config?.Reload();
+            InitConfig();
+        }
+
+        [HarmonyPatch(typeof(Workstation), "CraftItem")]
+        [HarmonyPostfix]
+        public static void CraftItem(ref Transform craftItem, ref bool __result)
+        {
+            if (returnBottle && __result)
+            {
+                var component = craftItem.GetComponent<Craftable>();
+                if (component.NeedTime > 0f)
+                {
+                    var itemRequirements = component.itemRequirements;
+                    foreach (var itemRequirement in itemRequirements)
+                    {
+                        if (itemRequirement.item.GetComponent<Item>().ItemID == RM.code.FreshWaterBottle.ItemID)
+                        {
+                            Transform itemTransform = Utility.Instantiate(RM.code.EmptyWaterBottle).transform;
+                            if (!Global.code.Player.playerStorage.AddItem(itemTransform))
+                            {
+                                Global.code.Player.quickSlotStorage.AddItem(itemTransform);
+                            }
+                            Global.code.uiCombat.RefreshQuickSlot();
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void CreateUI()
+        {
+            GameObject canvas = UIControls.createUICanvas();
+            uiPanel = UIControls.createUIPanel(canvas, "50", "240", Screen.width - 120, Screen.height - 75, null);
+            uiPanel.GetComponent<Image>().color = UIControls.HTMLString2Color("#00000000");
+
+            uiText = UIControls.createUIText(uiPanel, null, "#FFFFFFFF");
+            uiText.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+            text = uiText.GetComponent<Text>();
+            text.text = "";
+            text.fontSize = 20;
+            text.fontStyle = FontStyle.Bold;
+            text.alignment = TextAnchor.MiddleCenter;
+
+            uiArrow = UIControls.createUIText(uiPanel, null, "#FFFFFFFF");
+            arrowTransform = uiArrow.GetComponent<RectTransform>();
+            arrowTransform.localPosition = new Vector3(-90, 0, 0);
+            arrowTransform.sizeDelta = new Vector2(40, 40);
+            var arrowText = uiArrow.GetComponent<Text>();
+            arrowText.text = "↑";
+            arrowText.fontSize = 30;
+            arrowText.fontStyle = FontStyle.Bold;
+            arrowText.alignment = TextAnchor.MiddleCenter;
+        }
+
+        public static string GetNearestObject(Vector3 position)
+        {
+            var nearestDistanceSqr = float.PositiveInfinity;
+            nearestObj = null;
+            foreach (var collectable in WorldScene.code.worldCollectableContinuingInteractions)
+            {
+                if (collectable)
+                {
+                    float num = Vector3.Distance(collectable.transform.position, position);
+                    if (num < nearestDistanceSqr)
+                    {
+                        nearestDistanceSqr = num;
+                        nearestObj = collectable;
+                    }
+                }
+            }
+            foreach (var scavengeable in WorldScene.code.worldScavengables)
+            {
+                if (scavengeable)
+                {
+                    float num = Vector3.Distance(scavengeable.transform.position, position);
+                    if (num < nearestDistanceSqr)
+                    {
+                        nearestDistanceSqr = num;
+                        nearestObj = scavengeable;
+                    }
+                }
+            }
+            foreach (var breakable in WorldScene.code.worldBreakables)
+            {
+                if (breakable)
+                {
+                    float num = Vector3.Distance(breakable.transform.position, position);
+                    if (num < nearestDistanceSqr)
+                    {
+                        nearestDistanceSqr = num;
+                        nearestObj = breakable;
+                    }
+                }
+            }
+            foreach (var chest in WorldScene.code.worldChests)
+            {
+                if (chest)
+                {
+                    float num = Vector3.Distance(chest.transform.position, position);
+                    if (num < nearestDistanceSqr)
+                    {
+                        nearestDistanceSqr = num;
+                        nearestObj = chest;
+                    }
+                }
+            }
+            if (scanOre)
+            {
+                foreach (var choppable in WorldScene.code.choppables)
+                {
+                    if (choppable && choppable.M_ChoppableType != ChoppableType.Other)
+                    {
+                        float num = Vector3.Distance(choppable.transform.position, position);
+                        if (num < nearestDistanceSqr)
+                        {
+                            nearestDistanceSqr = num;
+                            nearestObj = choppable;
+                        }
+                    }
+                }
+            }
+            if (nearestDistanceSqr < 200)
+            {
+                var y = nearestObj.transform.position.y - position.y;
+                string sy;
+                if (y >= 0)
+                    sy = "↑";
+                else
+                {
+                    sy = "↓";
+                    y = -y;
+                }
+
+                return $"{nearestDistanceSqr:0.0}    {y:0}{sy}";
+            }
+            nearestObj = null;
+            return null;
+        }
+
+        public static void UpdateArrow()
+        {
+            if (nearestObj && uiArrow)
+            {
+                var vf = Vector3.ProjectOnPlane(FPSPlayer.code.transform.forward, Vector3.up);
+                var vt = Vector3.ProjectOnPlane(nearestObj.transform.position - FPSPlayer.code.transform.position, Vector3.up);
+                float num = Utility.ContAngle(vf, vt, Vector3.up);
+                arrowTransform.localRotation = Quaternion.Euler(0f, 0f, 0f - num);
+            }
+        }
+    }
+
+    public static class LoadConfig
+    {
+        public static ConfigEntry<int> StackAmount;
+        public static ConfigEntry<float> AirConsumtionRate;
+        public static ConfigEntry<float> EnergyConsumptionRate;
+        public static ConfigEntry<float> StaminaRecoveryRate;
+        public static ConfigEntry<int> Defence;
+        public static ConfigEntry<int> MaxItemsAmount;
+        public static ConfigEntry<bool> WorldSensor;
+        public static ConfigEntry<bool> ScanOre;
+        public static ConfigEntry<int> SensorSpan;
+        public static ConfigEntry<bool> DamageArmor;
+        public static ConfigEntry<bool> ReturnBottle;
+        const string Section = "General";
+        internal static void Init(ConfigFile config)
+        {
+            StackAmount = config.Bind(Section, nameof(StackAmount), defaultValue: 10, "堆叠倍数");
+            AirConsumtionRate = config.Bind(Section, nameof(AirConsumtionRate), defaultValue: 1f, "空气消耗率倍率");
+            EnergyConsumptionRate = config.Bind(Section, nameof(EnergyConsumptionRate), defaultValue: 1f, "能量消耗率倍率");
+            StaminaRecoveryRate = config.Bind(Section, nameof(StaminaRecoveryRate), defaultValue: 1f, "体力恢复率倍率");
+            Defence = config.Bind(Section, nameof(Defence), defaultValue: 0, "额外防御");
+            MaxItemsAmount = config.Bind(Section, nameof(MaxItemsAmount), defaultValue: 0, "额外物品栏");
+            WorldSensor = config.Bind(Section, nameof(WorldSensor), defaultValue: false, "启用世界探测器");
+            ScanOre = config.Bind(Section, nameof(ScanOre), defaultValue: true, "探测矿石");
+            SensorSpan = config.Bind(Section, nameof(SensorSpan), defaultValue: 30, "探测器间隔");
+            DamageArmor = config.Bind(Section, nameof(DamageArmor), defaultValue: true, "护甲损坏");
+            ReturnBottle = config.Bind(Section, nameof(ReturnBottle), defaultValue: false, "灶台返还水瓶");
+        }
+    }
+}
---
+++ SunkenlandUtil/UIControls.cs
@@ -0,0 +1,255 @@
+﻿using Fusion.StatsInternal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace UnityGameUI
+{
+    // UI 控件
+    internal class UIControls : MonoBehaviour
+    {
+        #region[声明]
+        private const float kWidth = 160f;
+        private const float kThickHeight = 30f;
+        private const float kThinHeight = 20f;
+        private static Vector2 s_ThickElementSize = new Vector2(160f, 30f);
+        private static Vector2 s_ThinElementSize = new Vector2(160f, 20f);
+        private static Vector2 s_ImageElementSize = new Vector2(100f, 100f);
+        private static Color s_DefaultSelectableColor = new Color(1f, 1f, 1f, 1f);
+        private static Color s_PanelColor = new Color(1f, 1f, 1f, 0.392f);
+        private static Color s_TextColor = new Color(0.19607843f, 0.19607843f, 0.19607843f, 1f);
+
+        public struct Resources
+        {
+            public Sprite standard;
+            public Sprite background;
+            public Sprite inputField;
+            public Sprite knob;
+            public Sprite checkmark;
+            public Sprite dropdown;
+            public Sprite mask;
+        }
+        #endregion
+
+        public UIControls()
+        {
+
+        }
+
+        #region[元素]
+
+        // 创建根元素
+        private static GameObject CreateUIElementRoot(string name, Vector2 size)
+        {
+            GameObject gameObject = new GameObject(name);
+            RectTransform rectTransform = gameObject.AddComponent<RectTransform>();
+            rectTransform.sizeDelta = size;
+            return gameObject;
+        }
+
+        // 创建UI对象
+        private static GameObject CreateUIObject(string name, GameObject parent)
+        {
+            GameObject gameObject = new GameObject(name);
+            gameObject.AddComponent<RectTransform>();
+            UIControls.SetParentAndAlign(gameObject, parent);
+            return gameObject;
+        }
+
+        // 设置默认文本
+        private static void SetDefaultTextValues(Text lbl)
+        {
+            lbl.color = UIControls.s_TextColor;
+            //lbl.AssignDefaultFont();
+            //lbl.FontTextureChanged();
+            lbl.font = (Font)UnityEngine.Resources.GetBuiltinResource<Font>("Arial.ttf");
+            // 设置字体为宋体
+            //lbl.font = (Font)UnityEngine.Resources.GetBuiltinResource<Font>("simkai.ttf");
+        }
+
+        // 设置默认颜色过度值
+        private static void SetDefaultColorTransitionValues(Selectable slider)
+        {
+            ColorBlock colors = slider.colors;
+            colors.highlightedColor = new Color(0.882f, 0.882f, 0.882f);
+            colors.pressedColor = new Color(0.698f, 0.698f, 0.698f);
+            colors.disabledColor = new Color(0.521f, 0.521f, 0.521f);
+        }
+
+        // 设置父级对其
+        private static void SetParentAndAlign(GameObject child, GameObject parent)
+        {
+            if (!(parent == null))
+            {
+                child.transform.SetParent(parent.transform, false);
+                UIControls.SetLayerRecursively(child, parent.layer);
+            }
+        }
+
+        // 递归设置层
+        private static void SetLayerRecursively(GameObject go, int layer)
+        {
+            go.layer = layer;
+            Transform transform = go.transform;
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                UIControls.SetLayerRecursively(transform.GetChild(i).gameObject, layer);
+            }
+        }
+
+        // 创建面板
+        public static GameObject CreatePanel(UIControls.Resources resources)
+        {
+            GameObject gameObject = UIControls.CreateUIElementRoot("Panel", UIControls.s_ThickElementSize);
+            RectTransform component = gameObject.GetComponent<RectTransform>();
+            component.anchorMin = Vector2.zero;
+            component.anchorMax = Vector2.one;
+            component.anchoredPosition = Vector2.zero;
+            component.sizeDelta = Vector2.zero;
+            Image image = gameObject.AddComponent<Image>();
+            image.sprite = resources.background;
+            image.type = Image.Type.Sliced;
+            image.color = UIControls.s_PanelColor;
+            return gameObject;
+        }
+
+        // 创建文本
+        public static GameObject CreateText(UIControls.Resources resources)
+        {
+            GameObject gameObject = UIControls.CreateUIElementRoot("Text", UIControls.s_ThickElementSize);
+            Text text = gameObject.AddComponent<Text>();
+            text.text = "New Text";
+            UIControls.SetDefaultTextValues(text);
+            return gameObject;
+        }
+
+        #endregion
+
+        #region[创建组件]
+
+        // 将 16进制格式颜色转换为Color32
+        public static Color32 HTMLString2Color(string htmlcolorstring)
+        {
+            #region[DevNote]
+            // Unity ref: https://docs.unity3d.com/ScriptReference/ColorUtility.TryParseHtmlString.html
+            // Note: Color strings can also set alpha: "#7AB900" vs. w/alpha "#7AB90003" 
+            //ColorUtility.TryParseHtmlString(htmlcolorstring, out color); // Unity's Method, This may have been stripped
+            #endregion
+
+            Color32 color = htmlcolorstring.HexToColor();
+
+            return color;
+        }
+
+        public static Texture2D createDefaultTexture(string htmlcolorstring)
+        {
+            Color32 color = HTMLString2Color(htmlcolorstring);
+
+            // Make a new sprite from a texture
+            Texture2D SpriteTexture = new Texture2D(1, 1);
+            SpriteTexture.SetPixel(0, 0, color);
+            SpriteTexture.Apply();
+
+            return SpriteTexture;
+        }
+
+        // 通过文件创建 Texture2D贴图格式
+        public static Texture2D createTextureFromFile(string FilePath)
+        {
+            // Load a PNG or JPG file from disk to a Texture2D
+            // 将 PNG 或 JPG 文件从磁盘加载到 Texture2D
+            Texture2D Tex2D;
+            byte[] FileData;
+
+            if (File.Exists(FilePath))
+            {
+                FileData = File.ReadAllBytes(FilePath);
+                Tex2D = new Texture2D(265, 198);
+                Tex2D.LoadRawTextureData(FileData);
+                //Tex2D.LoadImage(FileData, false);  // This is Broke. Unhollower/Texture2D doesn't like it...
+                Tex2D.Apply();
+                return Tex2D;
+            }
+            return null;
+        }
+
+        // 通过纹理贴图创建元素
+        public static Sprite createSpriteFrmTexture(Texture2D SpriteTexture)
+        {
+            // Create a new Sprite from Texture
+            Sprite NewSprite = Sprite.Create(SpriteTexture, new Rect(0, 0, SpriteTexture.width, SpriteTexture.height), new Vector2(0, 0), 100.0f, 0, SpriteMeshType.Tight);
+
+            return NewSprite;
+        }
+
+
+        // 创建画布
+        public static GameObject createUICanvas()
+        {
+            // Debug.Log("创建画布");
+
+            // Create a new Canvas Object with required components
+            GameObject CanvasGO = new GameObject("CanvasGO");
+            Object.DontDestroyOnLoad(CanvasGO);
+
+            // 传入 Canvas 类型
+            Canvas canvas = CanvasGO.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceCamera;
+
+            CanvasScaler cs = CanvasGO.AddComponent<CanvasScaler>();
+            cs.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+            cs.referencePixelsPerUnit = 100f;
+            cs.referenceResolution = new Vector2(1024f, 788f);
+
+            GraphicRaycaster gr = CanvasGO.AddComponent<GraphicRaycaster>();
+            ;
+            return CanvasGO;
+        }
+
+        // 创建UI面板
+        public static GameObject createUIPanel(GameObject canvas, string height, string width, float x, float y, Sprite BgSprite = null)
+        {
+            UIControls.Resources uiResources = new UIControls.Resources();
+
+            uiResources.background = BgSprite;
+
+            //log.LogMessage("   Creating UI Panel");
+            // Debug.Log("创建UI面板");
+            GameObject uiPanel = UIControls.CreatePanel(uiResources);
+            uiPanel.transform.SetParent(canvas.transform, false);
+
+            RectTransform rectTransform = uiPanel.GetComponent<RectTransform>();
+
+            float size;
+            size = Single.Parse(height); // 它们在 Unhollower 中没有浮动支持，这样可以避免错误
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size);
+            size = Single.Parse(width);
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size);
+            rectTransform.SetPositionAndRotation(new Vector3(x, y), default);
+            // 您也可以使用 rectTransform.sizeDelta = new Vector2(width, height);
+
+            return uiPanel;
+        }
+
+        // 创建文本
+        public static GameObject createUIText(GameObject parent, Sprite BgSprite, string textColor = null)
+        {
+            UIControls.Resources uiResources = new UIControls.Resources();
+            uiResources.background = BgSprite;
+
+            // Debug.Log("创建文本");
+            GameObject uiText = UIControls.CreateText(uiResources);
+            uiText.transform.SetParent(parent.transform, false);
+
+            //uiText.transform.GetChild(0).GetComponent<Text>().font = (Font)Resources.GetBuiltinResource<Font>("Arial.ttf"); // 设置字体
+            if (textColor != null) uiText.GetComponent<Text>().color = HTMLString2Color(textColor);
+            return uiText;
+        }
+
+        #endregion
+    }
+}
